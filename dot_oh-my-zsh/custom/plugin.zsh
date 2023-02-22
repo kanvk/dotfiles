da2fc995651270e5e57fe8fed33bc22478c51430
Kanv Khare <kkhare@ncsu.edu>
2023-02-22 14:21:14 -0500

    Fixed issue with zsh-autocomplete and ssh-agent

--- dot_oh-my-zsh/custom/plugin.zsh
+++ dot_oh-my-zsh/custom/plugin.zsh
@@ -1,5 +1,5 @@
 # marlonrichert/zsh-autocomplete
-zstyle ':autocomplete:*' insert-unambiguous yes # yes, no
+# zstyle ':autocomplete:*' insert-unambiguous yes # yes, no (breaks with menu-select)
 zstyle ':autocomplete:*' fzf-completion yes # yes, no
 zstyle ':autocomplete:*' widget-style menu-select # complete-word, menu-complete, menu-select
 zstyle ':autocomplete:*' min-delay 0.3  # seconds (float)
