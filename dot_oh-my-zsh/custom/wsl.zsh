f93382eda2068a23ff6edeebc3d9507aafa413cf
Kanv Khare <kkhare@ncsu.edu>
2023-02-22 17:29:00 -0500

    Added cmd/pwsh functions for WSL

--- dot_oh-my-zsh/custom/wsl.zsh
+++ dot_oh-my-zsh/custom/wsl.zsh
@@ -2,4 +2,15 @@
 alias exp='explorer.exe'
 alias yank='win32yank.exe -i --crlf'
 alias hx='hx.exe'
-alias strt='cmd.exe /C start'
+
+func strt() {
+    cmd.exe /C start "$*"
+}
+
+func wcmd() {
+    cmd.exe /C "$*"
+}
+
+func wpwsh() {
+    pwsh.exe -C "$*"
+}
