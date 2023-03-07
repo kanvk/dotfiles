b26a4fa7c4c78d339aa625db5ec9f8b34a498c91
Kanv Khare <kkhare@ncsu.edu>
2023-03-07 18:50:41 -0500

    Added aliases and modified PATH

--- dot_oh-my-zsh/custom/export.zsh
+++ dot_oh-my-zsh/custom/export.zsh
@@ -1,4 +1,4 @@
-export PATH=/home/kanvk/.local/bin:/home/kanvk/.cargo/bin:$PATH
+export PATH=/home/kanvk/.local/bin:/home/kanvk/.cargo/bin:/home/kanvk/go/bin:$PATH
 export EDITOR='lvim'
 export SUDO_EDITOR=/home/kanvk/.local/bin/lvim
 export GPG_TTY=$TTY
