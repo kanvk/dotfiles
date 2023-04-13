35215610a4b4688eff24b967049f067783ea793b
Kanv Khare <kkhare@ncsu.edu>
2023-04-13 02:33:06 -0400

    Added aliases for rust, go, and npm package updates

--- dot_oh-my-zsh/custom/alias.zsh
+++ dot_oh-my-zsh/custom/alias.zsh
@@ -1,19 +1,32 @@
+# web-search
 alias gle='google'
 alias wa='wolframalpha'
-alias py='python3'
+
+# Updates
 alias uup='sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y'
 alias zup='antigen update && omz update'
+alias rup='rustup update && cargo install-update -a'
+alias gup='go-global-update'
+alias nup='sudo npm -g update'
 alias vup='lvim +LvimUpdate +q'
+alias aup='uup && rup && gup && nup && vup && zup'
 alias uu='uup && zup'
+
+# vim
 alias nvim='lvim'
 alias vim='nvim'
 alias vimdiff='nvim -d'
 alias view='nvim -R'
+
+# Programs
+alias py='python3'
 alias bat='batcat'
+alias lg='lazygit'
+alias ldoc='lazydocker'
+alias clr='clear'
+
+# xclip
 alias c='xclip'
 alias v='xclip -o'
 alias csc='xclip -sel c'
 alias vsc='xclip -o -sel c'
-alias lg='lazygit'
-alias ldoc='lazydocker'
-alias clr='clear'
