e362466c14cb8dd6b9f9c34412a6e906f8da47d0
Kanv Khare <kkhare@ncsu.edu>
2023-04-26 22:21:19 -0400

    Set VISUAL to lvim

--- dot_oh-my-zsh/custom/export.zsh
+++ dot_oh-my-zsh/custom/export.zsh
@@ -1,4 +1,5 @@
 export PATH=/home/kanvk/.local/bin:/home/kanvk/.cargo/bin:/home/kanvk/go/bin:$PATH
+export VISUAL='lvim'
 export EDITOR='lvim'
 export SUDO_EDITOR=/home/kanvk/.local/bin/lvim
 export GPG_TTY=$TTY
