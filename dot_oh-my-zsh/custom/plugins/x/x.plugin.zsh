47213d03ee211be30ee359adc76977109df04236
Kanv Khare <kkhare@ncsu.edu>
2023-07-11 02:02:17 -0400

    Improved error handling for x

--- dot_oh-my-zsh/custom/plugins/x/x.plugin.zsh
+++ dot_oh-my-zsh/custom/plugins/x/x.plugin.zsh
@@ -1,31 +1,53 @@
 # Activates env
 function x() {
-  # Set environment variables
-  X_ENV=$1
-  X_ACTIVATE_PATH=$(pwd)
+  # Exit if env is already active
+  if [[ ! -z $X_ENV ]]; then
+      if [[ $X_ENV == $1  ]]; then
+        echo "$X_ENV already active" 1>&2
+      else
+        echo "$X_ENV is active, deactivate it first" 1>&2
+      fi
+      return 1
+  fi
 
-  # Already in env
-  if [[ -z $X_ENV ]]; then
-    echo "$X_ENV already active" 1>&2
-    return 1
-  # vv8-crawler-slim
-  elif [[ $X_ENV == "vv8" ]]; then
-    workon vv8
-  # triton
-  elif [[ $X_ENV == "triton" ]]; then
-    conda activate triton
-    cd ~/Documents/git/triton
-  # Env not found
-  else
-    echo "$X_ENV not found" 1>&2
-    unset X_ENV
-    unset X_ACTIVATE_PATH
+  # Exit if no env was provided
+  if [[ -z $1 ]]; then
+    echo "No environment provided" 1>&2
     return 1
   fi
+
+  # Temporarily store current directory
+  X_ACTIVATE_PATH_TMP=$(pwd)
+
+  # vv8-crawler-slim
+  if [[ $1 == "vv8" ]]; then
+    workon vv8
+  # triton
+  elif [[ $1 == "triton" ]]; then
+    conda activate triton
+    cd ~/Documents/git/triton
+    
+  # Env not found
+  else
+    echo "$1 not found" 1>&2
+    unset X_ACTIVATE_PATH_TMP
+    return 1
+  fi
+
+  # Set env vars
+  X_ENV=$1
+  X_ACTIVATE_PATH=$X_ACTIVATE_PATH_TMP
+  unset X_ACTIVATE_PATH_TMP
 }
 
 # Deactivates env
 function xd() {
+  # Exit if no env is active
+  if [[ -z $X_ENV ]]; then
+    echo "No environment active" 1>&2
+    return 1
+  fi
+
   # vv8-crawler-slim
   if [[ $X_ENV == "vv8" ]]; then
     deactivate
@@ -34,10 +56,7 @@ function xd() {
   elif [[ $X_ENV == "triton" ]]; then
     conda deactivate
     cd $X_ACTIVATE_PATH
-  # No env active
-  elif [[ $X_ENV == "" ]]; then
-    echo "No environment active" 1>&2
-    return 1
+
   # Env not found
   else
     echo "Unable to deactivate. $X_ENV not found" 1>&2
@@ -45,8 +64,7 @@ function xd() {
   fi
   
   # Unset environment variables
-  unset X_ENV
-  unset X_ACTIVATE_PATH
+  xc
 }
 
 # Clears environment variables
