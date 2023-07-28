8b1fe28b403b65c3b364b8367db0512b5d8ad353
Kanv Khare <kanvkhurana@hotmail.com>
2023-07-28 16:11:45 -0400

    Removed omz custom duplicates, added zshrc delims, updated lvim plugins

--- dot_local/share/sheldon/repos/github.com/ohmyzsh/ohmyzsh/custom/custom/source.zsh
+++
@@ -1,4 +0,0 @@
-source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
-source "$HOME/.asdf/asdf.sh"
-source /home/kanvk/.config/broot/launcher/bash/br
-source /home/kanvk/.spack/share/spack/setup-env.sh
