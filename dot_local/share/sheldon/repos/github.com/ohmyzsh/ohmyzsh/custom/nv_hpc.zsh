509e57f81a95cbd6858fe3eaa9e1e5487f8e9cc6
Kanv Khare <kanvkhurana@hotmail.com>
2024-02-20 17:34:19 -0500

    Updated nv_hpc config to work with newer library versions

--- dot_local/share/sheldon/repos/github.com/ohmyzsh/ohmyzsh/custom/nv_hpc.zsh
+++ dot_local/share/sheldon/repos/github.com/ohmyzsh/ohmyzsh/custom/nv_hpc.zsh
@@ -1,8 +1,9 @@
+NVVER=2024
 # Nvidia HPC SDK Config
 NVARCH=`uname -s`_`uname -m`; export NVARCH
 NVCOMPILERS=/opt/nvidia/hpc_sdk; export NVCOMPILERS
-MANPATH=$MANPATH:$NVCOMPILERS/$NVARCH/23.1/compilers/man; export MANPATH
-PATH=$NVCOMPILERS/$NVARCH/23.1/compilers/bin:$PATH; export PATH
+MANPATH=$MANPATH:$NVCOMPILERS/$NVARCH/$NVVER/compilers/man; export MANPATH
+PATH=$NVCOMPILERS/$NVARCH/$NVVER/compilers/bin:$PATH; export PATH
 # MPI Exports for Nvidia HPC SDK
-export PATH=$NVCOMPILERS/$NVARCH/23.1/comm_libs/mpi/bin:$PATH
-export MANPATH=$MANPATH:$NVCOMPILERS/$NVARCH/23.1/comm_libs/mpi/man
+export PATH=$NVCOMPILERS/$NVARCH/$NVVER/comm_libs/mpi/bin:$PATH
+export MANPATH=$MANPATH:$NVCOMPILERS/$NVARCH/$NVVER/comm_libs/mpi/man
