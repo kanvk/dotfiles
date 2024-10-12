7519d1b4f5cb7a348aaaa19762545b8dc67dd981
Kanv Khare <kanvkhurana@hotmail.com>
2024-10-12 19:02:28 -0400

    Disabled prompt hot reload, and made brew python auto-unlink

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -1829,7 +1829,7 @@ typeset -g POWERLEVEL9K_PER_DIRECTORY_HISTORY_GLOBAL_CONTENT_EXPANSION='G'
   # For example, you can type POWERLEVEL9K_BACKGROUND=red and see your prompt turn red. Hot reload
   # can slow down prompt by 1-2 milliseconds, so it's better to keep it turned off unless you
   # really need it.
-  typeset -g POWERLEVEL9K_DISABLE_HOT_RELOAD=true
+  typeset -g POWERLEVEL9K_DISABLE_HOT_RELOAD=false
 
   # If p10k is already loaded, reload configuration.
   # This works even with POWERLEVEL9K_DISABLE_HOT_RELOAD=true.
