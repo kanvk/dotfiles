f421f469b687c88843d2ade8351a85164ba33df6
kanvk <kanvkhurana@hotmail.com>
2026-01-28 17:38:07 -0500

    Added per-directory-history

--- dot_local/share/sheldon/repos/github.com/ohmyzsh/ohmyzsh/custom/plugin.zsh
+++ dot_local/share/sheldon/repos/github.com/ohmyzsh/ohmyzsh/custom/plugin.zsh
@@ -1,9 +1,9 @@
 # marlonrichert/zsh-autocomplete
 # zstyle ':autocomplete:*' insert-unambiguous yes # yes, no (breaks with menu-select)
-zstyle ':autocomplete:*' fzf-completion yes # yes, no
-zstyle ':autocomplete:*' widget-style menu-select # complete-word, menu-complete, menu-select
-zstyle ':autocomplete:*' min-delay 0.3  # seconds (float)
-bindkey '\t' menu-select "$terminfo[kcbt]" menu-select # Make tab go to the menu
+zstyle ':autocomplete:*' fzf-completion yes                                      # yes, no
+zstyle ':autocomplete:*' widget-style menu-select                                # complete-word, menu-complete, menu-select
+zstyle ':autocomplete:*' min-delay 0.3                                           # seconds (float)
+bindkey '\t' menu-select "$terminfo[kcbt]" menu-select                           # Make tab go to the menu
 bindkey -M menuselect '\t' menu-complete "$terminfo[kcbt]" reverse-menu-complete # Make tab cycle through the menu
 
 # colored-man-pages
@@ -11,3 +11,7 @@ less_termcap[so]="${fg_bold[yellow]}${bg[black]}" # Remove man page footer highl
 
 # djui/alias-tips
 export ZSH_PLUGINS_ALIAS_TIPS_EXCLUDES="_"
+
+# per-directory-history
+bindkey '^G' per-directory-history-toggle-history
+export HISTORY_START_WITH_GLOBAL=true
