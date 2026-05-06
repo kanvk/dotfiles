4ae41e805b7dee46fb7f3fd8423396cd4583a38f
kanvk <kanvkhurana@hotmail.com>
2026-05-05 20:18:37 -0400

    feat(windows): vendor PowerShell profile with lg/lzd aliases
    Adds the active Windows PowerShell profile under chezmoi management at
    dot_config/windows/powershell/profile.ps1, with new `lg` (lazygit) and
    `lzd` (lazydocker) aliases mirroring alias.zsh:163-164. Renders to
    ~/.config/windows/powershell/profile.ps1 on apply; manual copy to the
    Windows-side location per the existing dot_config/windows/ workflow.

---
+++ dot_config/windows/powershell/profile.ps1
@@ -0,0 +1,74 @@
+Import-Module 'C:\Program Files\gsudo\Current\gsudoModule.psd1'
+
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+function MSYS2-Update {
+    pacman -Syu --noconfirm
+}
+
+function Rust-Update {
+    rustup update && cargo install-update -a
+}
+
+function Texlive-Update {
+    sudo tlmgr update --self --all
+}
+
+function Pip-Upgrade {
+    uv tool upgrade --all
+}
+
+function UV-Update {
+    uv self update
+}
+
+function Go-Upgrade {
+    go-global-update
+}
+
+function TLDR-Update {
+    tldr -u
+}
+
+function Pyenv-Update {
+    &"${env:PYENV_HOME}\install-pyenv-win.ps1"
+}
+
+function uu {
+    Texlive-Update && UV-Update && Pip-Upgrade && Rust-Update && Go-Upgrade && TLDR-Update && MSYS2-Update
+}
+
+Set-Alias vim nvim
+Set-Alias lg lazygit
+Set-Alias lzd lazydocker
+Set-Alias mup MSYS2-Update
+Set-Alias rup Rust-Update
+Set-Alias tup Texlive-Update
+Set-Alias pup Pip-Upgrade
+Set-Alias goup Go-Upgrade
+Set-Alias tldrup TLDR-Update
+Set-Alias op start
+Set-Alias epl explorer
+Set-Alias clr clear
+Set-Alias llm ollama
+Set-Alias ouiup Open-WebUI-Update
+Set-Alias ts tailscale
+
+fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
+
+#region conda initialize
+# !! Contents within this block are managed by 'conda init' !!
+If (Test-Path "C:\ProgramData\miniforge3\Scripts\conda.exe") {
+    (& "C:\ProgramData\miniforge3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
+}
+#endregion
+
+#region mamba initialize
+# !! Contents within this block are managed by 'mamba shell init' !!
+$Env:MAMBA_ROOT_PREFIX = "C:\ProgramData\miniforge3"
+$Env:MAMBA_EXE = "C:\ProgramData\miniforge3\Library\bin\mamba.exe"
+(& $Env:MAMBA_EXE 'shell' 'hook' -s 'powershell' -r $Env:MAMBA_ROOT_PREFIX) | Out-String | Invoke-Expression
+#endregion
+
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/powerlevel10k_rainbow.omp.json" | Invoke-Expression
+Invoke-Expression (&starship init powershell)
