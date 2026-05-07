24761ac356e9b77bffda390c1ffdb836b5dc209f
kanvk <kanvkhurana@hotmail.com>
2026-05-06 21:05:04 -0400

    feat(lvim): editor add-ons

---
+++ dot_config/lvim/lua/plugins/editor.lua
@@ -0,0 +1,46 @@
+---@type LazySpec
+return {
+  {
+    "OXY2DEV/markview.nvim",
+    ft = { "markdown" },
+    keys = { { "<Leader>um", "<cmd>Markview Toggle<cr>", desc = "Toggle markview render" } },
+    opts = {},
+  },
+  {
+    "mrjones2014/smart-splits.nvim",
+    lazy = false,
+    keys = {
+      { "<C-h>", function() require("smart-splits").move_cursor_left() end, desc = "Window left" },
+      { "<C-j>", function() require("smart-splits").move_cursor_down() end, desc = "Window down" },
+      { "<C-k>", function() require("smart-splits").move_cursor_up() end, desc = "Window up" },
+      { "<C-l>", function() require("smart-splits").move_cursor_right() end, desc = "Window right" },
+    },
+  },
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    opts = {},
+  },
+  {
+    "NMAC427/guess-indent.nvim",
+    event = "BufReadPre",
+    opts = {},
+  },
+  { "nvim-treesitter/nvim-treesitter-context", event = "BufReadPre", opts = {} },
+  { "HiPhish/rainbow-delimiters.nvim", event = "BufReadPre" },
+  { "andymass/vim-matchup", event = "BufReadPre" },
+  {
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode",
+    keys = { { "<Leader>uz", "<cmd>ZenMode<cr>", desc = "Toggle Zen Mode" } },
+    opts = {},
+  },
+  {
+    "nvim-pack/nvim-spectre",
+    cmd = "Spectre",
+    keys = {
+      { "<Leader>sR", function() require("spectre").open() end, desc = "Spectre (project search-replace)" },
+    },
+    opts = {},
+  },
+}
